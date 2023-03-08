1526d25ee2f49f4fe1336d978ee8a5a9cb272e3c
laithalawad745 <laithalawad745@gmail.com>
2023-03-08 12:33:08 +0300

    gg

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,2 +1,18 @@
-﻿console.log("ad")
+﻿
+
+
+function getdata(){ 
+    let datas = new XMLHttpRequest()
+
+datas.onreadystatechange = function (){
+    
+    if(this.readyState === 4 && this.status === 200){
+        console.log(this.responseText)
+    }
+}
+
+datas.open("GET", "https://localhost:7210/api/APIconteroller", true);
+datas.send()
+
+ }
 
