e38a00a5f416673d07c1efa5e68e627922acf7d5
mmo2ze <99752390+Mmo2ze@users.noreply.github.com>
2023-03-13 20:15:39 +0200

    Update site.js

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,68 +1,60 @@
 ﻿let groub = document.getElementById("namegroub");
 let sentgroub = document.getElementById("sentgroub");
 
-sentgroub.addEventListener("click",()=>{
+sentgroub.addEventListener("click", () => {
 
-if(groub.value != ""){
-$.ajax({
-  url: "https://localhost:7210/Group",
-  type: "Post",
-  data: JSON.stringify({
-    name: groub.value,
-  }),
-  contentType: "application/json; charset=utf-8",
-  success: function (data) {
-    console.log(data);
-  },
-  error: function (er) {
-      console.log(er)
-  },
-});
+  if (groub.value != "") {
+    $.ajax({
+      url: "https://localhost:7210/Group",
+      type: "Post",
+      data: JSON.stringify({
+        name: groub.value,
+      }),
+      contentType: "application/json; charset=utf-8",
+      success: function (data) {
+        console.log(data);
+      },
+      error: function (er) {
+        console.log(er)
+      },
+    });
 
 
-}
+  }
 
 
 })
 
 
+let x = [];
 
 let getdata = document.getElementById("getgroub");
 let showdata = document.getElementById("showdata")
-let x 
 let d = document.getElementById("d");
 
-getdata.addEventListener("click", () => {
-
-$.ajax({
+getdata.addEventListener("click", async () => {
+  await $.ajax({
     url: "https://localhost:7210/Group",
     type: "Get",
     data: '', //{ Name: name, 
-     // Address: address, DOB: dob },
+    // Address: address, DOB: dob },
     contentType: 'application/json; charset=utf-8',
-    success: function (data) { 
-        x = data
-      console.log(x)},
+    success: function (data) {
+      x = data
+    },
     error: function (e) {
       console.log(e)
-     
-      }
-});
-
-
-for(let i = 0 ; i < x.length ; i++){
-
-
-head = document.createElement("tr");
-infodata = document.createElement("td");
-infodataloop = document.createTextNode(x[i].name);
-infodata.appendChild(infodataloop);
-head.appendChild(infodata)
-
-d.appendChild(head)
-
-  // showdata.innerHTML = x[i].name
-}
+    }
+  });
+  for (let i = 0; i < x.length; i++) {
+    head = document.createElement("tr");
+    infodata = document.createElement("td");
+    infodataloop = document.createTextNode(x[i].name);
+    infodata.appendChild(infodataloop);
+    head.appendChild(infodata)
+    d.appendChild(head)
+    // showdata.innerHTML = x[i].name
+  }
 
 });
 
